c61813a81478320bc96c73fd7571ed46e41cf0fa
Bilal Amiry <bezyy98@gmail.com>
2024-07-11 15:25:21 +0100

    TaxTask

--- src/main/scala/TaxCalculator.scala
+++ src/main/scala/TaxCalculator.scala
@@ -4,27 +4,64 @@ class TaxCalculator {
   private val personalAllowance: Int = 10000
   private val basicRateLimit: Int = 50000
   private val higherRateLimit: Int = 125000
+  private val capitalGainsTaxAllowance: Int = 3000
 
   // Tax rates
   private val personalAllowanceRate: Double = 0
   private val basicRate: Double = 0.2
   private val higherRate: Double = 0.4
   private val additionalRate: Double = 0.45
+  private val capitalGainsLowerTaxRate: Double = 0.1
+  private val capitalGainsHigherTaxRate: Double = 0.2
 
   // A method to calculate the total amount of tax to be paid, returned as a double
   def calculateTax(income: Double): Double = {
-    ???
+    if (income <= personalAllowance) {
+      personalAllowanceRate
+    } else if (income <= basicRateLimit) {
+      income * basicRate
+    } else if (income <= higherRateLimit) {
+      income * higherRate
+    } else {
+      income * additionalRate
+    }
   }
 
   // A method which can tell you if someone is a higher rate taxpayer
   def isHigherRateTaxpayer(income: Double): Boolean = {
-    ???
+    income > basicRateLimit && income <= higherRateLimit
   }
 
   // A method that will return a string with the income limit of their current tax band.
   // The return will also be formatted, E.g: "£12,500" or "No limit"
   def formattedCurrentTaxAllowance(income: Double): String = {
-    ???
+    if (income <= personalAllowance) {
+      "£10,000"
+    } else if (income <= basicRateLimit) {
+      "£50,000"
+    } else if (income <= higherRateLimit) {
+      "£125,000"
+    } else {
+      "No limit"
+    }
   }
+
+
+  // A method to calculate the capital gains tax, returned as a double
+  def calculateCapitalGainsTax(capitalGains: Double, income: Double): Double = {
+    if (capitalGains < capitalGainsTaxAllowance) {
+      0
+    } else if (income < basicRateLimit) {
+      capitalGains * capitalGainsLowerTaxRate
+    } else {
+      capitalGains * capitalGainsHigherTaxRate
+    }
+  }
+
+  // method to calculate total tax
+  def calculateTotalTax(income: Double, capitalGains: Double): Double = {
+    calculateTax(income) + calculateCapitalGainsTax(capitalGains, income)
+  }
+
 
 }
--- src/test/scala/TaxCalculatorSpec.scala
+++ src/test/scala/TaxCalculatorSpec.scala
@@ -5,14 +5,107 @@ class TaxCalculatorSpec extends AnyWordSpec {
 
   val taxCalculator: TaxCalculator = new TaxCalculator
 
-  // I've done the first test for you!
   "TaxCalculator.calculateTax" should {
     "return the total amount of tax to pay" when {
       "the income is below the personal tax limit" in {
         val result: Double = taxCalculator.calculateTax(5000)
-
         assert(result == 0)
       }
+      "the income is in the range of basic tax limit" in {
+        val income = 12000
+        val basicTaxRate = 0.2
+        val result: Double = taxCalculator.calculateTax(income)
+        assert(result == income * basicTaxRate)
+      }
+      "the income is in the range of higher tax limit" in {
+        val income = 70000
+        val higherTaxRate = 0.4
+        val result: Double = taxCalculator.calculateTax(income)
+        assert(result == income * higherTaxRate)
+      }
+      "the income is in the range of additional tax limit" in {
+        val income = 150000
+        val additionalTaxRate = 0.45
+        val result: Double = taxCalculator.calculateTax(income)
+        assert(result == income * additionalTaxRate)
+      }
+    }
+  }
+
+  "TaxCalculator.isHigherRateTaxpayer" should {
+    "return true" when {
+      "the income is above the higher rate limit" in {
+        val result: Boolean = taxCalculator.isHigherRateTaxpayer(100000)
+        assert(result)
+      }
+    }
+
+    "return false" when {
+      "the income is below or more the higher rate limit" in {
+        val result: Boolean = taxCalculator.isHigherRateTaxpayer(40000)
+        assert(!result)
+      }
+    }
+  }
+
+  "TaxCalculator.formattedCurrentTaxAllowance" should {
+    "return the correct formatted string" when {
+      "the income is below the personal tax limit" in {
+        val result: String = taxCalculator.formattedCurrentTaxAllowance(5000)
+        assert(result == "£10,000")
+      }
+
+      "the income is within the basic rate limit" in {
+        val result: String = taxCalculator.formattedCurrentTaxAllowance(20000)
+        assert(result == "£50,000")
+      }
+
+      "the income is within the higher rate limit" in {
+        val result: String = taxCalculator.formattedCurrentTaxAllowance(120000)
+        assert(result == "£125,000")
+      }
+
+      "the income is above the higher rate limit" in {
+        val result: String = taxCalculator.formattedCurrentTaxAllowance(130000)
+        assert(result == "No limit")
+      }
+    }
+  }
+  "TaxCalculator.calculateCapitalGainsTax" should {
+    "return the total tax on capital gains" when {
+      "capital gains are below the allowance" in {
+        val result: Double = taxCalculator.calculateCapitalGainsTax(2000, 20000)
+        assert(result == 0)
+      }
+      "income is bellow basic rate" in {
+        val capitalGains = 4000
+        val income = 20000
+        val taxRate = 0.1
+        val result: Double = taxCalculator.calculateCapitalGainsTax(capitalGains, income)
+        assert(result == capitalGains * taxRate)
+      }
+      "income is above basic rate" in {
+        val capitalGains = 4000
+        val income = 60000
+        val taxRate = 0.2
+        val result: Double = taxCalculator.calculateCapitalGainsTax(capitalGains, income)
+        assert(result == capitalGains * taxRate)
+      }
+    }
+  }
+  "TaxCalculator.calculateTotalTax" should {
+    "calculate total income tax AND capital gains tax from shares" when {
+      "the income is below the personal tax limit and below personal capital gains allowance" in {
+        val income = 20000
+        val capitalGains = 5000
+        val expectedIncomeTax = taxCalculator.calculateTax(income)
+        val expectedCapitalGainsTax = taxCalculator.calculateCapitalGainsTax(capitalGains, income)
+        val expectedTotalTax = expectedIncomeTax + expectedCapitalGainsTax
+        val result: Double = taxCalculator.calculateTotalTax(income, capitalGains)
+        assert(result == expectedTotalTax)
+      }
+
+
     }
   }
 }
